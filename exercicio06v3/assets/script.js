a7c074ec2904b2b61715a760809c4a29a99bc6b2
victor <joaovicif2021@gmail.com>
2023-09-12 17:17:51 -0300

    populando grid com itens de src aleatórios

--- exercicio06v3/assets/script.js
+++ exercicio06v3/assets/script.js
@@ -1,16 +1,17 @@
-const divs = document.querySelectorAll('.grid-item');
 const tempoSpan = document.querySelector('#cronometro');
 const botaoComecar = document.querySelector('.start');
+const mainGrid = document.querySelector('.grid-container');
+const sources = [
+    '../assets/ratata.jpeg',
+    '../assets/fletchling.jpeg',
+    '../assets/pidgey.jpeg',
+    '../assets/pikachu.jpeg',
+    '../assets/geodude.jpeg',
+    '../assets/ekans.jpeg',
+    '../assets/charmander.jpeg',
+    '../assets/bulbasaur.png'
+];
 
-divs.forEach(div => {
-    div.addEventListener('click', () => {
-        if (div.classList.contains('virada')) {
-            div.classList.remove('virada');
-        } else {
-            div.classList.add('virada');
-        };
-    });
-});
 
 tempo = 20;
 
@@ -20,4 +21,44 @@ const iniciarCronometro = () => {
     tempo--;
 }
 
-setInterval(iniciarCronometro, 1000);
+//setInterval(iniciarCronometro, 1000);
+
+const gerarNumAleatorio = () => {
+    return Math.floor(Math.random() * sources.length)
+}
+
+const popularGrid = (numCartas) => {
+    for (let i = 0; i < numCartas; i++) {
+        setTimeout('400');
+        mainGrid.innerHTML += createGridItem(sources[gerarNumAleatorio()]);
+    }
+};
+
+const createGridItem = (source) => {
+    return `
+    <div class="grid-item">
+    <div class="imagem-container">
+    <div class="imagem-frente">
+    <img src="../assets/carta-virada.jpeg" alt=''>
+    </div>
+    
+    <div class="imagem-costa">
+    <img src='${source}' alt=''>
+    </div>
+    </div>
+    </div>
+    `
+}
+
+popularGrid(16);
+const divs = document.querySelectorAll('.grid-item');
+divs.forEach(div => {
+    div.addEventListener('click', () => {
+        console.log('a');
+        if (div.classList.contains('virada')) {
+            div.classList.remove('virada');
+        } else {
+            div.classList.add('virada');
+        };
+    });
+});
