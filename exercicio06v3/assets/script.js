d6e9be1973025117ec43e0a4b1618867e054eb04
victor <joaovicif2021@gmail.com>
2023-12-06 10:18:36 -0300

    completa a lógica do jogo

--- exercicio06v3/assets/script.js
+++ exercicio06v3/assets/script.js
@@ -1,4 +1,4 @@
-const tempoSpan = document.querySelector('#cronometro');
+const scoreSpan = document.querySelector('#score');
 const botaoRecomecar = document.querySelector('.restart');
 const mainGrid = document.querySelector('.grid-container');
 const container = document.querySelector('.container');
@@ -34,6 +34,10 @@ let cardList = [
     '../assets/bulbasaur.png'
 ];
 
+let firstCard, secondCard;
+let gridBloqueado = false;
+let score = 0;
+let matchSpree = 0;
 const geraNumAleatorio = (limite) => {
     return Math.floor(Math.random() * limite);
 }
@@ -50,38 +54,130 @@ const embaralhaCartas = (list) => {
 
 const createGridItem = (source) => {
     return `
-    <div class="grid-item">
-        <div class="imagem-container">
-            <div class="imagem-frente">
-                <img src="../assets/carta-virada.jpeg" alt=''>
-            </div>
-    
-            <div class="imagem-costa">
-                <img src='${source}' alt=''>
-            </div>
+    <div class="imagem-container">
+        <div class="imagem-frente">
+            <img src="../assets/carta-virada.jpeg" alt=''>
+        </div>
+
+        <div class="imagem-costa">
+            <img src='${source}' alt=''>
         </div>
     </div>
     `
 };
 
 const popularGrid = () => {
+    mainGrid.innerHTML = '';
     shuffledCardList = embaralhaCartas(cardList);
     for (let i = 0; i < shuffledCardList.length; i++) {
-        mainGrid.innerHTML += createGridItem(shuffledCardList[i]);
-    }
+        let cardItem = document.createElement('div');
+
+        cardItem.classList.add('grid-item');
+        cardItem.setAttribute('card-src', shuffledCardList[i]);
+        cardItem.innerHTML = createGridItem(shuffledCardList[i]);
+        cardItem.addEventListener('click', flipCard);
+        mainGrid.appendChild(cardItem);
+    };
 };
-window.onload = popularGrid;
 
-botaoRecomecar.addEventListener('click', popularGrid);
+const restartGame = () => {
+    resetBoard();
+    popularGrid();
+    //addFlipOnClick();
+    score = 0;
+    scoreSpan.innerHTML = score;
+}
 
-const divs = document.querySelectorAll('.grid-item');
-divs.forEach(div => {
-    div.addEventListener('click', () => {
-        console.log('a');
-        if (div.classList.contains('virada')) {
-            div.classList.remove('virada');
-        } else {
-            div.classList.add('virada');
-        };
-    });
-});
+window.onload = restartGame;
+
+botaoRecomecar.addEventListener('click', restartGame);
+
+console.log(document.querySelectorAll('.grid-item'));
+
+// const addFlipOnClick = () => {
+//     const divs = document.querySelectorAll('.grid-item');
+//     divs.forEach(div => {
+//         div.addEventListener('click', () => {
+//             if (div.classList.contains('virada')) {
+//                 div.classList.remove('virada');
+//             } else {
+//                 div.classList.add('virada');
+//             };
+//         });
+//     });
+// }
+
+function flipCard() {
+    if (gridBloqueado) return;
+    if (this === firstCard) return;
+    if (checkIfGameIsOver()) {
+        setTimeout(gameOver, 100);
+    }
+    this.classList.add('virada');
+
+    if (!firstCard) {
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    gridBloqueado = true;
+    buscaMatch();
+}
+
+const buscaMatch = () => {
+    let isMatch = firstCard.getAttribute("card-src") === secondCard.getAttribute("card-src");
+    // isMatch ? disableCards() : unflipCards();
+    if (isMatch == true) {
+        matchSpree++;
+        score = score + matchSpree * 10;
+        scoreSpan.innerHTML = score;
+        disableCards();
+    } else {
+        matchSpree = 0;
+        unflipCards();
+    }
+}
+
+const disableCards = () => {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', secondCard);
+
+    resetBoard();
+}
+
+const unflipCards = () => {
+    setTimeout(() => {
+        firstCard.classList.remove('virada');
+        secondCard.classList.remove('virada');
+        resetBoard();
+    }, 1000);
+}
+
+const resetBoard = () => {
+    firstCard = null;
+    secondCard = null;
+    gridBloqueado = false;
+}
+
+const checkIfGameIsOver = () => {
+    let cardItems = document.querySelectorAll('.grid-item');
+    let numeroDeCartasViradas = 0;
+    cardItems.forEach(cardItem => {
+        if (cardItem.classList.contains('virada')) {
+            numeroDeCartasViradas++;
+        }
+    })
+
+    console.log(numeroDeCartasViradas);
+    if (numeroDeCartasViradas == 15) {
+        return true;
+    }
+
+    return false;
+}
+
+const gameOver = () => {
+    alert(`Game Over. pontuação: ${score}`);
+    restartGame();
+}
