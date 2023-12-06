f26f02bdd2d63a78b4d8cb6ad9e78c43280b686c
victor <joaovicif2021@gmail.com>
2023-12-06 08:50:24 -0300

    feita função para popular o grid

--- exercicio06v3/assets/script.js
+++ exercicio06v3/assets/script.js
@@ -1,5 +1,5 @@
 const tempoSpan = document.querySelector('#cronometro');
-const botaoComecar = document.querySelector('.start');
+const botaoRecomecar = document.querySelector('.restart');
 const mainGrid = document.querySelector('.grid-container');
 const container = document.querySelector('.container');
 
@@ -38,11 +38,38 @@ const geraNumAleatorio = (limite) => {
     return Math.floor(Math.random() * limite);
 }
 
-const embaralhaCartas = (cardList) => {
+const embaralhaCartas = (list) => {
     //algoritmo de fisher-yates
-    for (let i = 0; i < cardList.length; i++) {
-        let k = geraNumAleatorio(cardList.length);
-        [cardList[i], cardList[k]] = [cardList[k], cardList[i]];
+    for (let i = 0; i < list.length; i++) {
+        let k = geraNumAleatorio(list.length);
+        [list[i], list[k]] = [list[k], list[i]];
     }
-}
 
+    return list;
+};
+
+const createGridItem = (source) => {
+    return `
+    <div class="grid-item">
+        <div class="imagem-container">
+            <div class="imagem-frente">
+                <img src="../assets/carta-virada.jpeg" alt=''>
+            </div>
+    
+            <div class="imagem-costa">
+                <img src='${source}' alt=''>
+            </div>
+        </div>
+    </div>
+    `
+};
+
+const popularGrid = () => {
+    shuffledCardList = embaralhaCartas(cardList);
+    for (let i = 0; i < shuffledCardList.length; i++) {
+        mainGrid.innerHTML += createGridItem(shuffledCardList[i]);
+    }
+};
+window.onload = popularGrid;
+
+botaoRecomecar.addEventListener('click', popularGrid);
