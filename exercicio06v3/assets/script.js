2a3e146f7cc9a4b81c68fd877ee27ff1a200431f
victor <joaovicif2021@gmail.com>
2023-12-06 08:34:12 -0300

    implementando algoritmo de fisher-yates para embaralhar as cartas

--- exercicio06v3/assets/script.js
+++ exercicio06v3/assets/script.js
@@ -11,5 +11,40 @@ const sources = [
     '../assets/geodude.jpeg',
     '../assets/ekans.jpeg',
     '../assets/charmander.jpeg',
+    '../assets/bulbasaur.png',
+    
+];
+
+let cardList = [
+    '../assets/ratata.jpeg',
+    '../assets/fletchling.jpeg',
+    '../assets/pidgey.jpeg',
+    '../assets/pikachu.jpeg',
+    '../assets/geodude.jpeg',
+    '../assets/ekans.jpeg',
+    '../assets/charmander.jpeg',
+    '../assets/bulbasaur.png',
+    '../assets/ratata.jpeg',
+    '../assets/fletchling.jpeg',
+    '../assets/pidgey.jpeg',
+    '../assets/pikachu.jpeg',
+    '../assets/geodude.jpeg',
+    '../assets/ekans.jpeg',
+    '../assets/charmander.jpeg',
     '../assets/bulbasaur.png'
 ];
+
+const geraNumAleatorio = (limite) => {
+    return Math.floor(Math.random() * limite);
+}
+
+const embaralhaCartas = (cardList) => {
+    //algoritmo de fisher-yates
+    for (let i = 0; i < cardList.length; i++) {
+        let k = geraNumAleatorio(cardList.length);
+        [cardList[i], cardList[k]] = [cardList[k], cardList[i]];
+    }
+}
+
+embaralhaCartas(cardList);
+console.log(cardList);
