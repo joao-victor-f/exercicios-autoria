5b7cdedbddf9a67f35e95b4518a75cea0206a898
victor <joaovicif2021@gmail.com>
2023-12-12 17:28:36 -0300

    aplicada persistência no exercicio11

--- exercicio11/assets/script.js
+++ exercicio11/assets/script.js
@@ -4,6 +4,29 @@ const inputNota = document.getElementById('nota');
 const dataTable = document.getElementById('dadosTable');
 const form = document.querySelector('form');
 
+var alunosData = [];
+if (localStorage.getItem("alunos-data")) {
+    alunosData = JSON.parse(localStorage.getItem("alunos-data"));
+}
+
+class Aluno {
+    constructor(nome, nota) {
+        this.nome = nome;
+        this.nota = nota;
+    }
+};
+
+
+for (var alunoData of alunosData) {
+    if (alunoData) {
+        dataTable.innerHTML += 
+            `<tr>
+                <td align="center">${alunoData.nome}</th>
+                <td align="center">${alunoData.nota}</td>
+            </tr>`
+    }
+}
+
 btnConfirmar.addEventListener('click', (e) => {
     let valido = true;
     if (inputNota.value > 100 || inputNota.value < 0) {
@@ -17,13 +40,20 @@ btnConfirmar.addEventListener('click', (e) => {
     }
 
     if (valido) {
+        e.preventDefault();
+        const aluno = new Aluno(inputNome.value, inputNota.value);
+
+        alunosData.push(aluno);
+
+        localStorage.setItem("alunos-data", JSON.stringify(alunosData));
         dataTable.innerHTML += `
         <tr>
             <td align="center">${inputNome.value}</th>
             <td align="center">${inputNota.value}</td>
         </tr>
-        `
-        e.preventDefault();
+        `;
+
+        
     }
 
     form.reset();
